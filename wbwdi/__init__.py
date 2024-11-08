02b6f7ced201b16d5226849bc1753733f07fb6c1
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2024-11-08 12:28:05 +0100

    Finalize wdi_search

--- wbwdi/__init__.py
+++ wbwdi/__init__.py
@@ -6,3 +6,4 @@ from .wdi_get_lending_types import wdi_get_lending_types
 from .wdi_get_languages import wdi_get_languages
 from .wdi_get_geographies import wdi_get_geographies
 from .wdi_get_indicators import wdi_get_indicators
+from .wdi_search import wdi_search
--- wbwdi/wdi_search.py
+++ wbwdi/wdi_search.py
@@ -7,7 +7,7 @@ def wdi_search(data, keywords, columns=None):
 
     This function searches for specified keywords across columns in a DataFrame
     and returns only the rows where at least one keyword appears in any of the
-    specified columns. It supports nested columns (lists within columns) and
+    specified columns. It does not support nested columns (lists within columns) and
     provides the option to limit the search to specific columns.
 
     Parameters:
@@ -26,27 +26,31 @@ def wdi_search(data, keywords, columns=None):
     >>> indicators = wdi_get_indicators()
 
     Search for keywords "inequality" or "gender" across all columns
-    >>> wdi_search(
-    ...     indicators,
-    ...     keywords=["inequality", "gender"]
-    ... )
+    >>> wdi_search(indicators, keywords=["inequality", "gender"])
 
     Search for keywords only within the "indicator_name" column
-    >>> wdi_search(
-    ...     indicators,
-    ...     keywords=["inequality", "gender"],
-    ...     columns=["indicator_name"]
-    ... )
+    >>> wdi_search(indicators, keywords=["inequality", "gender"], columns=["indicator_name"])
     """
+
+    if not keywords or (columns is not None and not columns):
+        return data.head(0)
+
     if not isinstance(keywords, list) or not all(isinstance(k, str) for k in keywords):
         raise TypeError("`keywords` must be a list of strings.")
 
     if columns is not None:
         if not isinstance(columns, list) or not all(isinstance(c, str) for c in columns):
             raise TypeError("`columns` must be a list of strings or `None`.")
+        for col in columns:
+            if isinstance(data[col].dtype, (pl.List, pl.Struct)):
+                raise ValueError(f"Column '{col}' has unsupported type {data[col].dtype}. Struct or list columns are currently not supported.")
+        columns_to_search = columns
+    else:
+        columns_to_search = [
+            col for col in data.columns if not isinstance(data[col].dtype, (pl.List, pl.Struct))
+        ]
 
-    columns_to_search = columns if columns else data.columns
-    conditions = [pl.col(col).str.contains_any(keywords) for col in columns_to_search]
+    conditions = [pl.col(col).cast(pl.Utf8).str.contains_any(keywords, ascii_case_insensitive=True) for col in columns_to_search]
     data_filtered = data.filter(reduce(lambda acc, cond: acc | cond, conditions))
     
     return data_filtered
